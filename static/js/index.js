1c08a49061582d6547b02114343ef1065e2bafa0
Thyago <thiagogg020676@gmail.com>
2024-08-01 22:35:38 -0300

    remove section works, add section clients

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,14 @@
 // import { request } from "./data/request.js";
 import services from "./modules/carouselServices.js";
-import work from "./modules/carouselWork.js";
-import modal from "./components/modal.js";
+// import work from "./modules/carouselWork.js";
+// import modal from "./components/modal.js";
 // import createServices from "./components/sectionServices.js";
 services();
-work()
+// work()
 
-document.addEventListener('DOMContentLoaded', () => {
-  modal();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   modal();
+// });
 // document.addEventListener("DOMContentLoaded", () => {
 
 //   // Requisição de serviços
